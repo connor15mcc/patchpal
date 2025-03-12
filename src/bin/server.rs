fbf589e7d3f2c0778e052942f1cf8609e42092fc
Connor McCarthy <connor15mcc@gmail.com>
2025-03-11 23:50:21 -0400

    add TUI module for counter, use mpsc for patches

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -17,56 +17,50 @@
 //! connected clients they'll all join the same room and see everyone else's
 //! messages.
 
-use std::{
-    collections::HashMap,
-    env,
-    io::Error as IoError,
-    net::SocketAddr,
-    sync::{Arc, Mutex},
-};
+use patchpal::{models::patchpal::Patch, tui};
+use std::{env, io::Error as IoError, net::SocketAddr};
 
-use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_channel::mpsc::unbounded;
 use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
 
-use tokio::net::{TcpListener, TcpStream};
+use log::info;
+use tokio::{
+    net::{TcpListener, TcpStream},
+    sync::mpsc::{channel, Sender},
+};
 use tokio_tungstenite::tungstenite::protocol::Message;
 
 use prost::Message as _;
 
-type Tx = UnboundedSender<Message>;
-type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
-
-async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr) {
-    println!("Incoming TCP connection from: {}", addr);
+async fn handle_connection(raw_stream: TcpStream, addr: SocketAddr, tx: Sender<Patch>) {
+    info!("Incoming TCP connection from: {}", addr);
 
     let ws_stream = tokio_tungstenite::accept_async(raw_stream)
         .await
         .expect("Error during the websocket handshake occurred");
-    println!("WebSocket connection established: {}", addr);
+    info!("WebSocket connection established: {}", addr);
 
     // Insert the write part of this peer to the peer map.
-    let (tx, rx) = unbounded();
-    peer_map.lock().unwrap().insert(addr, tx);
+    let (_tx, rx) = unbounded();
 
     let (outgoing, incoming) = ws_stream.split();
 
-    let broadcast_incoming = incoming.try_for_each(|msg| match msg {
-        Message::Binary(b) => {
-            let patch = patchpal::models::patchpal::Patch::decode(b).unwrap();
-            println!("Received a message from {}: {}", addr, patch.metadata,);
-            let peers = peer_map.lock().unwrap();
+    let broadcast_incoming = incoming.try_for_each(async |msg| {
+        match msg {
+            Message::Binary(b) => {
+                let patch = Patch::decode(b).unwrap();
+                info!("Received a message from {}: {}", addr, patch.metadata);
+                tx.send(patch).await.unwrap();
+                info!("Sent state update from addr {}", addr);
 
-            let _broadcast_recipients = peers
-                .iter()
-                .filter(|(peer_addr, _)| peer_addr != &&addr)
-                .map(|(_, ws_sink)| ws_sink);
-
-            future::ok(())
+                future::ok(())
+            }
+            _ => {
+                eprintln!("Not sure how to handle... (got non-binary message)");
+                future::ok(())
+            }
         }
-        _ => {
-            eprintln!("Not sure how to handle... (got non-binary message)");
-            future::ok(())
-        }
+        .await
     });
 
     let receive_from_others = rx.map(Ok).forward(outgoing);
@@ -74,27 +68,46 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: Socke
     pin_mut!(broadcast_incoming, receive_from_others);
     future::select(broadcast_incoming, receive_from_others).await;
 
-    println!("{} disconnected", &addr);
-    peer_map.lock().unwrap().remove(&addr);
+    info!("{} disconnected", &addr);
 }
 
-#[tokio::main]
-async fn main() -> Result<(), IoError> {
+async fn run_patch_server(tx: Sender<Patch>) -> Result<(), IoError> {
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
-    let state = PeerMap::new(Mutex::new(HashMap::new()));
-
     // Create the event loop and TCP listener we'll accept connections on.
     let try_socket = TcpListener::bind(&addr).await;
     let listener = try_socket.expect("Failed to bind");
-    println!("Listening on: {}", addr);
+    info!("Listening on: {}", addr);
 
     // Let's spawn the handling of each connection in a separate task.
     while let Ok((stream, addr)) = listener.accept().await {
-        tokio::spawn(handle_connection(state.clone(), stream, addr));
+        info!("Accepted listener as {}", addr);
+        tokio::spawn(handle_connection(stream, addr, tx.clone()));
     }
 
     Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    cli_log::init_cli_log!();
+
+    let mut terminal = ratatui::init();
+    // arbitrarily decided: should think about this more
+    // can maybe even just use oneshot channel
+    let (tx, mut rx) = channel::<Patch>(10);
+
+    let runtime = tokio::runtime::Runtime::new()?;
+    runtime.spawn(run_patch_server(tx));
+
+    let app_result = tui::App::default().run(&mut terminal, &mut rx);
+    ratatui::restore();
+    app_result?;
+    Ok(())
+    //run_tui().await;
+    //cli_log::init_cli_log!();
+    //
+    //let (_patch_srv, _tui) =
+    //    tokio::join!(tokio::spawn(run_patch_server()), tokio::spawn(run_tui()));
 }
