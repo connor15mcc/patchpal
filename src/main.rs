9bd9618f776035e9007c8097b7279d34487a18a3
Connor McCarthy <connor15mcc@gmail.com>
2025-03-20 23:13:45 -0400

    configure verbosity of loggers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::fs::File;
+
 use args::Command;
 use clap::Parser;
-use log::debug;
+use log::{debug, LevelFilter};
+use simplelog::{Config, WriteLogger};
 mod args;
 mod client;
 mod models;
@@ -10,19 +13,32 @@ mod tui;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = args::Cli::try_parse()?;
-
-    stderrlog::new()
-        .verbosity(cli.verbose as usize)
-        .module(module_path!())
-        .init()
-        .unwrap();
+    let verbosity = cli.verbose as usize;
+    let level_filter = match &verbosity {
+        0 => LevelFilter::Off,
+        1 => LevelFilter::Warn,
+        2 => LevelFilter::Info,
+        _ => LevelFilter::Debug,
+    };
 
     match cli.command() {
         Command::Client(mode) => {
+            stderrlog::new()
+                .verbosity(verbosity)
+                .module(module_path!())
+                .init()
+                .unwrap();
+
             debug!("Starting client");
             client::Client::from(mode).run().await?;
         }
         Command::Server => {
+            WriteLogger::init(
+                level_filter,
+                Config::default(),
+                File::create("patchpal.log").unwrap(),
+            )?;
+
             debug!("Starting server");
             server::Server::new().run().await?;
         }
